07a3bc4dcc1bac3675211e1f91f9a78f5c46eca4
mpassos75 <m.passos@outlook.com>
2026-05-12 22:49:14 -0300

    style.css

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.sendFile('style.css', { root: 'public/css' });
+    res.sendFile('style.css', { root: 'css' });
     res.sendFile('estacionar.jpg', { root: 'public' });
     res.sendFile('coment.txt', { root: 'public' });
     res.sendFile('script.js', { root: 'public/src' });
